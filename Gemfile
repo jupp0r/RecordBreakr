26d0f888a21287cd5dd6beca2009bbc95aaa61d0
Jupp Müller <jupp0r@gmail.com>
2012-07-04 13:16:55 +0200

    initial commit

---
+++ Gemfile
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+# vim: et ts=2 sw=2
+
+source "https://rubygems.org"
+
+gem "sinatra", "~> 1.3.2"
+#gem "sass", "~> 3.1.17"
+gem "coffee-script", "~> 2.2.0"
+gem "health_graph", :git=>"git://github.com/adammck/health_graph.git"
+
+gem "thin", "~> 1.3.1"
+gem "shotgun", "~> 0.9"
---
+++ app.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+# vim: et ts=2 sw=2
+
+require "set"
+require "date"
+require "health_graph"
+require "sinatra"
+
+HealthGraph.configure do |config|
+  config.client_id = ENV["CLIENT_ID"]
+  config.client_secret = ENV["CLIENT_SECRET"]
+  config.authorization_redirect_url = ENV["REDIRECT_URL"]
+end
+
+get "/" do
+  token = request.cookies["token"]
+  redirect "/auth" unless token
+
+  user = HealthGraph::User.new token
+end
+
+get "/auth" do
+  redirect HealthGraph.authorize_url
+end
+
+get "/callback" do
+  token = HealthGraph.access_token params[:code]
+  response.set_cookie "token", token
+  redirect "/"
+end
