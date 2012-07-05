b4d7c899ca87026deb60b3d6240a0714c49e199c
Jupp Müller <jupp0r@gmail.com>
2012-07-05 09:47:12 +0200

    working, performance enhancements, pretty time and distance formating

--- Gemfile
+++ Gemfile
@@ -7,7 +7,7 @@ gem "sinatra", "~> 1.3.2"
 #gem "sass", "~> 3.1.17"
 gem "haml", "~> 3.1.6"
 gem "coffee-script", "~> 2.2.0"
-gem "health_graph", :git=>"git://github.com/jupp0r/health_graph.git"
+gem "health_graph", :path => "../health_graph"
 
 gem "thin", "~> 1.3.1"
 gem "shotgun", "~> 0.9"
--- app.rb
+++ app.rb
@@ -13,6 +13,66 @@ HealthGraph.configure do |config|
   config.authorization_redirect_url = ENV["REDIRECT_URL"]
 end
 
+helpers do
+  def calculate_fastest_distance target_distance, fitness_activity
+    #assuming sorted distance map here
+    #fitness_activity.distance.map! do |point|
+    #  point.timestamp = point.timestamp.to_f
+    #  point.distance = point.distance.to_f
+    #end
+    return nil unless target_distance < fitness_activity.distance.last.distance
+    fastest_race_time = Float::INFINITY
+    fastest_race_start = 0.0
+    fastest_race_stop = 0.0
+    fitness_activity.distance.each do |start_point|
+      last_end_point = start_point
+      fitness_activity.distance.each do |end_point|
+        if start_point.timestamp < end_point.timestamp
+          distance_start_to_end = end_point.distance - start_point.distance
+          if target_distance <= distance_start_to_end
+            # linear interpolation of intermediate time
+            error_distance = distance_start_to_end - target_distance
+            diff_time = end_point.timestamp - last_end_point.timestamp
+            diff_distance = end_point.distance - last_end_point.distance
+            stop_time = last_end_point.timestamp + diff_time*(diff_distance/error_distance)
+            current_race_time =  stop_time - start_point.timestamp
+            fastest_race_time = current_race_time if current_race_time < fastest_race_time
+            fastest_race_start = start_point.timestamp
+            fastest_race_stop = stop_time
+            break
+          end
+        end
+        last_end_point = end_point
+      end
+    end
+    {:time => fastest_race_time, :start => fastest_race_start, :stop => fastest_race_stop}
+  end
+
+  def parse_duration duration_in_s
+    duration_in_s = (duration_in_s+0.5).to_i
+    hours = duration_in_s/3600
+    minutes = (duration_in_s % 3600) / 60
+    seconds = duration_in_s % 60
+    {:hours => hours, :minutes => minutes, :seconds => seconds}
+  end
+
+  def format_duration duration
+    converted = parse_duration duration
+    output_str = "%02d" % converted[:seconds]
+    output_str = "%02d:" % converted[:minutes] + output_str unless converted[:minutes] == 0 and converted[:hours] == 0
+    output_str =  "#{converted[:hours]}:" + output_str unless converted[:hours] == 0
+    output_str
+  end
+
+  def format_distance distance
+    "%#.2f km" % (distance/1000)
+  end
+
+  def calculate_average_pace total_distance, duration
+    duration*1000/total_distance
+  end
+end
+
 get "/" do
   token = request.cookies["token"]
   redirect "/auth" unless token
@@ -26,6 +86,21 @@ get "/" do
   while feed
     @fitness_items += feed.items
     feed = feed.next_page
+  end
+
+  @activities = []
+
+  @fitness_items.each do |fitness_item|
+    @activities.push fitness_item.item
+  end
+
+  @records = Hash.new
+
+  [1000, 5000, 10000, 210974, 42195].each do |distance|
+    @records[distance] = Hash.new
+    @activities.each do |activity|
+      @records[distance][activity] = calculate_fastest_distance distance, activity
+    end
   end
 
   haml :index, :format => :html5
