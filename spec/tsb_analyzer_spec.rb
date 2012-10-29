b7275cd6504c2f7859c434c60eeaee55e90369bc
Jupp Müller <jupp0r@gmail.com>
2012-10-29 11:03:17 +0100

    added pending specs for atl and ctl

---
+++ spec/tsb_analyzer_spec.rb
@@ -0,0 +1,28 @@
+require_relative './rspec_helpers'
+require_relative '../lib/tsb_analyzer'
+
+describe TsbAnalyzer do
+  describe "#atl" do
+    it "should calculate atl for no workouts" do
+      pending
+    end
+    it "should calculate atl for a single workout" do
+      pending
+    end
+    it "should calculate atl for multiple workouts" do
+      pending
+    end
+  end
+
+  describe "#ctl" do
+    it "should calculate atl for no workouts" do
+      pending
+    end
+    it "should calculate atl for a single workout" do
+      pending
+    end
+    it "should calculate atl for multiple workouts" do
+      pending
+    end
+  end
+end
