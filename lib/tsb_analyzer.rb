31c170ac203f357e44592e4b4bd70eddc3334f13
Jupp Müller <jupp0r@gmail.com>
2012-10-31 01:35:38 +0100

    refactoring TsbAnalyzer

--- lib/tsb_analyzer.rb
+++ lib/tsb_analyzer.rb
@@ -13,10 +13,7 @@ class TsbAnalyzer
     lambda_a = 2.0/(7+1)
     k_a = 1.0
     start_date.upto end_date do |day|
-      trimp_today = 0.0
-      @activities.find_all{|activity| activity.start_time.to_date == day}.each do |activity|
-        trimp_today = trimp_today + activity.trimp
-      end
+      trimp_today = trimp_for_day day
       atl_today = trimp_today * lambda_a * k_a + ((1.0 - lambda_a) * atl_yesterday)
       atl_yesterday = atl_today
       atl_values[day] = atl_today
@@ -30,10 +27,7 @@ class TsbAnalyzer
     k_f = 2.0
     ctl_yesterday = 0.0
     start_date.upto end_date do |day|
-      trimp_today = 0.0
-      @activities.find_all{|activity| activity.start_time.to_date == day}.each do |activity|
-        trimp_today = trimp_today + activity.trimp
-      end
+      trimp_today = trimp_for_day day
       ctl_today = trimp_today * lambda_f * k_f + ((1.0 - lambda_f) * ctl_yesterday)
       ctl_yesterday = ctl_today
       ctl_values[day] = ctl_today
@@ -45,13 +39,19 @@ class TsbAnalyzer
     return 0.0 if @activities.empty?
     trimp_vector = []
     (date-6).upto date do |day|
-      trimp_today = 0.0
-      @activities.find_all{|activity| activity.start_time.to_date == day}.each do |activity|
-        trimp_today = trimp_today + activity.trimp
-      end
-      trimp_vector.push trimp_today
+      trimp_vector.push(trimp_for_day day)
     end
     trimp_vector.extend(DescriptiveStatistics)
     trimp_vector.mean / trimp_vector.standard_deviation
+  end
+
+  private
+
+  def trimp_for_day day
+    trimp_today = 0.0
+    @activities.find_all{|activity| activity.start_time.to_date == day}.each do |activity|
+      trimp_today = trimp_today + activity.trimp
+    end
+    trimp_today
   end
 end
