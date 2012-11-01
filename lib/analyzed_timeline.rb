8f2f935e26b607f5ec594e50279bbf4362d51ee2
Jupp Müller <jupp0r@gmail.com>
2012-11-01 23:40:27 +0100

    added spec and class skeleton for analyzed timeline

---
+++ lib/analyzed_timeline.rb
@@ -0,0 +1,3 @@
+class AnalyzedTimeline
+
+end
---
+++ spec/analyzed_timeline_spec.rb
@@ -0,0 +1,11 @@
+require 'rspec'
+
+require_relative '../lib/analyzed_timeline'
+
+describe AnalyzedTimeline do
+  it "should return atl for all days"
+  it "should return ctl for all days"
+  it "should return ctl-atl for all days"
+  it "should return maximum cumulative distances"
+  it "sholud return records for all activities"
+end
